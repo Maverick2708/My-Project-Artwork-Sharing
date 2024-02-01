c2723049ea26061c60c7ab8037a780868189e77f
Maverick2708 <daigiakien02@gmail.com>
2024-02-01 14:18:04 +0700

    Update ConnectionString and DB

---
+++ API_ArtworkSharingPlatforms/API_ArtworkSharingPlatform.Repository/Migrations/20240201071549_UpdateDB.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_ArtworkSharingPlatform.Repository.Migrations
+{
+    public partial class UpdateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsConfirm",
+                table: "Person",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsConfirm",
+                table: "Person");
+        }
+    }
+}
