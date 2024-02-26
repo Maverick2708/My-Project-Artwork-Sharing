287d9b29d2f9093fbb64e481a4745484ec944db1
uyentramle <uyentramle1010@gmail.com>
2024-02-26 13:35:20 +0700

    Create new classes for Order Feature

---
+++ API_ArtworkSharingPlatforms/API_ArtworkSharingPlatform.Repository/Interfaces/IOrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using API_ArtworkSharingPlatform.Repository.Data;
+using API_ArtworkSharingPlatform.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_ArtworkSharingPlatform.Repository.Interfaces
+{
+	public interface IOrderRepository
+	{
+		public Task<Order> GetOrderById(int BillOrderId);
+		public Task<IEnumerable<Order>> GetArtworkByUser(string UserId);
+		public Task<IEnumerable<Order>> GetOrders();
+		public Task<Order> CreateOrder(Order order);
+		public Task<bool> UpdateOrder(Order order);
+		public Task<bool> DeleteOrder(int billOrderId);
+
+	}
+}
