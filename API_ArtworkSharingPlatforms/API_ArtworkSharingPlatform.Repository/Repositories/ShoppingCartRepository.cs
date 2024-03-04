d141324066c88aafc1bcd7b01e1a2f0c2f4802a7
uyentramle <uyentramle1010@gmail.com>
2024-03-04 16:17:06 +0700

    ShoppingCart update

--- API_ArtworkSharingPlatforms/API_ArtworkSharingPlatform.Repository/Repositories/ShoppingCartRepository.cs
+++ API_ArtworkSharingPlatforms/API_ArtworkSharingPlatform.Repository/Repositories/ShoppingCartRepository.cs
@@ -1,4 +1,6 @@
 ﻿using API_ArtworkSharingPlatform.Repository.Interfaces;
+using API_ArtworkSharingPlatform.Repository.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,23 @@ namespace API_ArtworkSharingPlatform.Repository.Repositories
 {
 	public class ShoppingCartRepository : IShoppingCartRepository
 	{
+		private readonly Artwork_SharingContext _context;
+
+		public ShoppingCartRepository(Artwork_SharingContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<ShoppingCart> GetShoppingCartById(int shoppingCartId)
+		{
+			return await _context.ShoppingCarts.FindAsync(shoppingCartId);
+		}
+
+		public async Task<IEnumerable<ShoppingCart>> GetShoppingCartByUser(string userId)
+		{
+			return await _context.ShoppingCarts
+				.Where(cart => cart.UserId == userId)
+				.ToListAsync();
+		}
 	}
 }
---
+++ API_ArtworkSharingPlatforms/API_ArtworkSharingPlatforms/Controllers/ShoppingCartController.cs
@@ -0,0 +1,53 @@
+﻿using API_ArtworkSharingPlatform.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_ArtworkSharingPlatforms.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ShoppingCartController : Controller
+	{
+		private readonly IShoppingCartService _shoppingCartService;
+
+		public ShoppingCartController(IShoppingCartService shoppingCartService)
+		{
+			_shoppingCartService = shoppingCartService;
+		}
+
+		[HttpGet("{shoppingCartId}")]
+		public async Task<IActionResult> GetShoppingCartById(int shoppingCartId)
+		{
+			try
+			{
+				var shoppingCart = await _shoppingCartService.GetShoppingCartById(shoppingCartId);
+				if (shoppingCart == null)
+					return NotFound();
+
+				return Ok(shoppingCart);
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+			}
+		}
+
+		[HttpGet("user/{userId}")]
+		public async Task<IActionResult> GetShoppingCartByUser(string userId)
+		{
+			try
+			{
+				var shoppingCarts = await _shoppingCartService.GetShoppingCartByUser(userId);
+				if (shoppingCarts == null || !shoppingCarts.Any())
+					return NotFound();
+
+				return Ok(shoppingCarts);
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+			}
+		}
+
+	}
+}
